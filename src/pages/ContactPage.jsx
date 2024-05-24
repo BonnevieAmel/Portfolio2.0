19fa16d3409468fdbfeb8ce77ae5f1f65656269f
BonnevieAmel <amel.bonnevie@gmail.com>
2024-05-24 10:29:39 +0200

    Cv add

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,39 @@
-import ContactForm from "../components/ContactForm";
+import cv from "../assets/Cv.png";
 
 function ContactPage() {
   return (
-    <section className="ContactPageComponent">
-      <h1>ContactPage</h1>
-      <ContactForm />
-    </section>
+    <>
+      <div className="contact-container">
+        <address>
+          <a href="tel:0678921722">06.78.92.17.22</a>
+
+          <a href="mailto:amel.bonnevie@gmail.com" target="_blank">
+            Email
+          </a>
+
+          <a href="https://github.com/BonnevieAmel" target="_blank">
+            {" "}
+            Github{" "}
+          </a>
+          {""}
+          <a
+            href="https://www.linkedin.com/in/amel-bonnevie-54099833/"
+            target="_blank"
+          >
+            Linkedin
+          </a>
+        </address>
+      </div>
+      <div>
+        <h2
+          className="title-h2
+        "
+        >
+          Mon CV
+        </h2>
+        <img className="cv-img" src={cv} alt="cv" />
+      </div>
+    </>
   );
 }
 
